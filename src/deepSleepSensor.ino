a0b5bed4c78b248e095fd181085c288e06536e7b
AlradeAbdullah <Alrade.Abdullah@gmail.com>
2021-05-08 17:28:47 +0200

    add SD_FILElibrary

--- src/deepSleepSensor.ino
+++ src/deepSleepSensor.ino
@@ -6,8 +6,6 @@
   Henry Bahnasawy (91elhe1bif@hft-stuttgart.de)
 *********/
 
-
-
 // Libraries for the SD card
 #include "FS.h"
 #include "SD.h"
@@ -19,25 +17,12 @@
 // Libraries for WiFi and HTTP
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <SD_file.h>
 
-//User_info
- static String name;
 // Define deep sleep options
-uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
-// Sleep for 10 seconds
-static uint64_t TIME_TO_SLEEP = 10;
-//Time TIME_TO_recording and Sampling_Rate
-static uint64_t TIME_TO_recording ;
-static uint64_t Sampling_Rate ;
-
-
-// Replace with your network credentials
-static String config_SSID ="bemmon";
-static  String config_Passowrd ="bemoon";
-
-
+uint64_t uS_TO_S_FACTOR = 1000000;
+  
 bool debug = true;
-
 #define DHTPIN 0               // DHT Pin 0
 #define DHTTYPE DHT22         // DHT 22  (AM2302)
 
@@ -58,7 +43,6 @@ DHT dht(DHTPIN, DHTTYPE);
 #define I2S_DOUT      35
 
 String dataMessage;
-String content;
 
 // Temperature and Humidity Sensor variables
 float temperature;
@@ -83,118 +67,18 @@ boolean print_wakeup_reason() {
   }
 }
 
-//_______________________________________________________________getValue
-String getValue(String data, char separator, int index)
-{
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
 
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
-    }
-  }
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-  }
-//_______________________________________________________________________readFileconfig
-void readFileconfig(fs::FS &fs, const char * path) {
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file) {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read variable from  config_file: \n");
-  String line="";
-  
-  while (file.available()) {
-       char c =  file.read();
-        line +=c;
-  }
-  file.close();
-   name = (getValue(line,';',0));
-   config_SSID = getValue(line,';',1).c_str();
-   config_Passowrd = getValue(line,';',2).c_str();
-   TIME_TO_SLEEP =getValue(line,';',4).toInt();
-   TIME_TO_recording = getValue(line,';',5).toInt();
-   Sampling_Rate = getValue(line,';',6).toInt();
-   
-   
-
-
-}
-// Write to the SD card
-void writeFile(fs::FS &fs, const char * path, const char * message) {
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file) {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if (file.print(message)) {
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-// Append data to the SD card
-void appendFile(fs::FS &fs, const char * path, const char * message) {
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if (!file) {
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if (file.print(message)) {
-    Serial.println("Message appended");
-  } else {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
-// rename file on SD card
-void renameFile(fs::FS &fs, String path1, String path2) {
-  Serial.println("Renaming file " + path1 + " to " + path2);
-  if (fs.rename(path1, path2)) {
-    Serial.println("File renamed");
-  } else {
-    Serial.println("Rename failed");
-  }
-}
-// Read file sensor.txt from the SD card
-void readFile(fs::FS &fs, const char * path) {
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file) {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read from file: ");
-  while (file.available()) {
-    content += (char)file.read();
-  }
-  file.close();
-}
 //_____________________________________________________________________________setup
 void setup() {
+  default_config("Bemoon", 20,5,4410,"bemoon","bemmon");
+  // content;
   // Start serial communication for debugging purposes
   Serial.begin(115200);
   delay(1000); //Take some time to open up the Serial Monitor
-  boolean button = print_wakeup_reason();
 
+  boolean button = print_wakeup_reason();
   // Initialize SD card
-  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+ SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
   pinMode(SD_CS, OUTPUT);
   if (!SD.begin()) {
     Serial.println("Card Mount Failed");
@@ -210,7 +94,7 @@ void setup() {
     Serial.println("ERROR - SD card initialization failed!");
     return;    // init failed
   }
-   readFileconfig(SD, "/config.txt");
+    readFileconfig(SD, "/config.txt");
 
   // If the sensor.txt file doesn't exist
   // Create a file on the SD card
@@ -231,7 +115,7 @@ void setup() {
   delay(4000); // 4 seconds pre-reading time for the sensor (the sensor is sometimes a little slow)
 
   if (button) { // check if KEY1 was pressed
-    wifiConnect(config_SSID.c_str() ,config_Passowrd.c_str());
+    wifiConnect(SSID.c_str() ,Passowrd.c_str());
     sendDataHTTP();
   } else {
     getReadings();
@@ -243,7 +127,6 @@ void setup() {
   // Start deep sleep
   Serial.println("DONE! Going to sleep now.");
     Serial.println("________________________________________");
-
   
   esp_deep_sleep_start();
 }
